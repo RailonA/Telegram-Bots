f471ae42e736dfdda525958fc2293a52e1cfd3ab
RailonA <railonacosta@gmail.com>
2023-04-04 19:48:54 -0400

    Fx news bot tweeting

--- scripts/twitter_API.js
+++ scripts/twitter_API.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const { TwitterApi } = require('twitter-api-v2')
     
 const client = new TwitterApi({
-  appKey: 'process.env.TWITTER_CONSUMER_KEY',
-  appSecret: 'process.env.TWITTER_CONSUMER_SECRET',
-  accessToken: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
-  accessSecret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET',
+  appKey: process.env.TWITTER_CONSUMER_KEY,
+  appSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const rwClient = client.readWrite
